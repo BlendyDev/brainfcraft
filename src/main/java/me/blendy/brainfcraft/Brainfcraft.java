b9bbe0d3aed0c6e516fc226e1e4fcb1c6603e5cb
BlendyDev <blendydev@gmail.com>
2023-09-29 23:16:43 +0200

    feat: prefix

--- src/main/java/me/blendy/brainfcraft/Brainfcraft.java
+++ src/main/java/me/blendy/brainfcraft/Brainfcraft.java
@@ -1,13 +1,14 @@
 package me.blendy.brainfcraft;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public final class Brainfcraft extends JavaPlugin {
-
+    public static String prefix = ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "Brainfcraft" + ChatColor.DARK_GREEN + "] " + ChatColor.RESET;
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length != 2) return true;
@@ -15,7 +16,7 @@ public final class Brainfcraft extends JavaPlugin {
             @Override
             public void run() {
                 Interpreter interpreter = new InterpreterImpl(1000, 1000, args[0].toCharArray(), args[1].toCharArray());
-                Bukkit.broadcastMessage(interpreter.process());
+                Bukkit.broadcastMessage(prefix + interpreter.process());
             }
         }.runTaskAsynchronously(this);
         return true;
